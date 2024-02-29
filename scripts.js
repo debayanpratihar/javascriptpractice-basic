37b976f231638e2bc44571f3f4843110bf06dafb
Debayan Pratihar <121422042+debayanpratihar@users.noreply.github.com>
2024-02-29 11:31:26 +0530

    calculator

--- scripts.js
+++ scripts.js
@@ -278,3 +278,31 @@ console.log(typeof heros);
 console.log(typeof value);
 console.log(typeof myFunction);
 console.log(typeof food);
+
+// program for a simple calculator
+
+// take the operator input
+const operator = prompt('Enter operator ( either +, -, * or / ): ');
+
+// take the operand input
+const number1 = parseFloat(prompt('Enter first number: '));
+const number2 = parseFloat(prompt('Enter second number: '));
+
+let result;
+
+// using if...else if... else
+if (operator == '+') {
+    result = number1 + number2;
+}
+else if (operator == '-') {
+    result = number1 - number2;
+}
+else if (operator == '*') {
+    result = number1 * number2;
+}
+else {
+    result = number1 / number2;
+}
+
+// display the result
+console.log(`${number1} ${operator} ${number2} = ${result}`);
